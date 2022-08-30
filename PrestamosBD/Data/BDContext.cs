96adad2aebc0f54308297788077cf8765a303371
Hoffman Erwin <erwinhoffman03@gmail.com>
2022-08-30 00:11:10 -0300

    Agregar archivos de proyecto.

---
+++ PrestamosBD/Data/BDContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrestamosBD.Data.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestamosBD.Data
+{
+    public class BDContext : DbContext
+    {
+        public BDContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Usuario> TablaUsuario { get; set; }
+    }
+}
---
+++ PrestamosBD/Data/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestamosBD.Data.Entidades
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
