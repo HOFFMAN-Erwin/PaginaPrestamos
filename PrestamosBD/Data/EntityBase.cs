38794546ce2140b237b60ab09587528244ade544
Hoffman Erwin <erwinhoffman03@gmail.com>
2022-08-30 02:34:08 -0300

    EntityBase clase

---
+++ PrestamosBD/Data/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PrestamosBD.Data
+{
+    [Index(nameof(Id), Name = "IdUsuario_UQ", IsUnique = true)]
+    public class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
